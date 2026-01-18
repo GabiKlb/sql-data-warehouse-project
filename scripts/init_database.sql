75437f3b647856090a265ce8c32dbba6d4c0b5c7
Gabriele <121892766+GabiKlb@users.noreply.github.com>
2026-01-18 22:27:38 +0100

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,9 @@
+-- create database
+CREATE DATABASE datawarehouse;
+
+-- create schemas
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
+
+-- this script creates a new database with 3 new schemas: bronze, silver, gold
